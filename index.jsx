5c271d5c8348b94152aeade05bce7dc4eb3f87bd
raihan-778 <raihanarif778@gmail.com>
2022-10-25 19:22:42 +0600

    topics and catagery api is created

--- index.jsx
+++ index.jsx
@@ -1,10 +1,34 @@
 const express = require("express");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
   console.log(`web & cyber-security el is running on Port ${port}`)
 );
+//api for topic category
+
+const topics = require("./data/topics.json");
+const topicInfo = require("./data/topicDetails.json");
+
+app.get("/topics-category", (req, res) => {
+  res.send({ topics });
+});
+
+//api for single topic
+app.get("/topics-category/:id", (req, res) => {
+  const id = req.params.id;
+  const topicDetails = topicInfo.filter(
+    (singleTopic) => singleTopic.category_id === id
+  );
+  res.send(topicDetails);
+});
+
+//api for all topics topicDetails
+
+app.get("/allTopics", (req, res) => {
+  res.send({ topicInfo });
+});
 
 // create a GET route
 app.get("/express_backend", (req, res) => {
